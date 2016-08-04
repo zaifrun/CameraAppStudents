2c664925fbb3071d42e18e0479a9d21830a24807
Martin Knudsen <martinknud@gmail.com>
2016-08-04 15:31:57 +0200

    added comments.

--- app/src/main/java/org/projects/cameraapp/MainActivity.java
+++ app/src/main/java/org/projects/cameraapp/MainActivity.java
@@ -23,17 +23,25 @@ public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
     ImageView imageView; //for displaying the image.
-    String mCurrentPhotoPath;
+    String mCurrentPhotoPath; //for storing the path to the image taken.
 
     private File createImageFile() throws IOException {
-        // Create an image file name
+        // Create an image file name - we want a unique filename
+        //so we are using the current time as a timestamp to include in the filename.
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
+        //This specifies that we store the images in the PRIVATE folder
+        //that ONLY our app has access to (this is the effect of getExternalFilesDir).
+        //and the Environment.DIRECTORY_PICTURES constant will make sure that
+        //we store the file in the subdirectory called "Pictures" within that
+        //directory.
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
 
+        //you can take a look at this path in the android monitor - it will give you
+        //some idea of what the path looks like - this is a VERY good idea.
         System.out.println("storagedirectory:"+storageDir);
 
-        //Environment.DIRECTORY_PICTURES);
+        //This creates an empty file.
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
                 ".jpg",         /* suffix */
@@ -49,9 +57,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-
+            //NOTICE we do not get any data directly back from the intent
+            //but the camera app will take the picture and save it in the url
+            //we have specified earlier and we can use that path to read the file
+            //and convert it into a bitmap.
             if (mCurrentPhotoPath!=null) {
+                //decoding the file into a bitmap
                 Bitmap imageBitmap = BitmapFactory.decodeFile(mCurrentPhotoPath);
+                //setting the bitmap on the image file.
                 imageView.setImageBitmap(imageBitmap);
             }
             else {
@@ -64,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //Make sure we have an app that can hanlde the IMAGE_CAPTURE intent.
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             // Create the File where the photo should go
             File photoFile = null;
@@ -76,6 +90,8 @@ public class MainActivity extends AppCompatActivity {
             }
             // Continue only if the File was successfully created
             if (photoFile != null) {
+                //This converts our file to a URI, using the provider we have
+                //made in the manifest file.
                 Uri photoURI = FileProvider.getUriForFile(this,
                         "org.projects.cameraapp.fileprovider",
                         photoFile);
@@ -85,6 +101,7 @@ public class MainActivity extends AppCompatActivity {
 
         } else
         {
+            //Make a toast if there is no camera app installed.
             Toast toast = Toast.makeText(this,"No program to take pictures",Toast.LENGTH_SHORT);
             toast.show();
         }
