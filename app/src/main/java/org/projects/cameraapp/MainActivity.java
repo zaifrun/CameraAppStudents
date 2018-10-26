8cc0d0650b01e4bf262d478fc25d6f72bb6d84db
makn <makn@eaaa.dk>
2018-10-26 14:08:41 +0200

    added clear button to clear image

--- app/src/main/java/org/projects/cameraapp/MainActivity.java
+++ app/src/main/java/org/projects/cameraapp/MainActivity.java
@@ -146,6 +146,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button clearButton = findViewById(R.id.clearButton);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageResource(0);
+            }
+        });
+
+
+
 
 
     }
